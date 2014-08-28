0b7a096efbf5ae0efe11939605dc1309440870aa
jpardobl <kit2707888>
2014-08-28 19:35:47 +0200

    made button desapear

--- hautomation_gpio/utils.py
+++ hautomation_gpio/utils.py
@@ -4,11 +4,11 @@ from hautomation_gpio import USED_GPIO_LIST
 
 
 def is_input(address):
-    return RPIO.gpio_function(address) == RPIO.IN
+    return RPIO.gpio_function(int(address)) == RPIO.IN
 
 
 def is_output(address):
-    return RPIO.gpio_function(address) == RPIO.OUT
+    return RPIO.gpio_function(int(address)) == RPIO.OUT
 
 
 def validate_address(address):
