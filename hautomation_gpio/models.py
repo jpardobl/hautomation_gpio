0b2667bf6654b3430411d921a2a0e257878f346a
jpardobl <kit2707888>
2014-08-30 10:45:44 +0200

    made button desapear

--- hautomation_gpio/models.py
+++ hautomation_gpio/models.py
@@ -1,6 +1,4 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from hacore.models import Device
+
 from hautomation_gpio.cmds import set_to_output
 from hautomation_gpio.utils import is_output
 import logging
@@ -14,11 +12,18 @@ except:
 logger.setLevel(settings.LOG_LEVEL)
 
 
+try:
+    from django.db.models.signals import post_save
+    from django.dispatch import receiver
+    from hacore.models import Device
 
-@receiver(post_save, sender=Device)
-def post_save_gpio_device(sender, instance, **kwargs):
-    if instance.protocol.name == "GPIO" and not is_output(instance.did):
-        logger.debug("Just saved GPIO device not OUT-configured, changing it to OUT...")
-        set_to_output(instance.did)
+    @receiver(post_save, sender=Device)
+    def post_save_gpio_device(sender, instance, **kwargs):
+        if instance.protocol.name == "GPIO" and not is_output(instance.did):
+            logger.debug("Just saved GPIO device not OUT-configured, changing it to OUT...")
+            set_to_output(instance.did)
+except Exception:
+    logger.debug("No suppor for Django detected, thus not activating hacore.models.Device post_save signal")
+    pass
 
 
